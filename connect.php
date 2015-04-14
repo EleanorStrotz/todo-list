062c52d082249c2c75ccb6c3d89947d17301dfc8
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-14 11:25:59 -0700

    video 4 almost done

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+//linked to localhost to successfully create database
+//creates a secure connection
+//created the database mysqli
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+
+// 4, 3:31
+?>
