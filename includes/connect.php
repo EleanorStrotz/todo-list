a48cd669c90f8b97d368c398817c93603c4aa6a7
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-15 11:09:11 -0700

    milestone 5.1 error fixed

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
 //linked to localhost to successfully create database
 //creates a secure connection
 //created the database mysqli
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 //$mysqli->connect_error then we want is to die and have this message
 if ($mysqli->connect_error) {
 	die('Connect Error(' . $mysqli->connect_errno . ')'
